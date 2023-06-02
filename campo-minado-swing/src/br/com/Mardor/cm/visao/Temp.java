dfc5b2bee42908963741f066e715096bccac0fa5
Marcelo M Dorneles <marcelo.mal.dor@gmail.com>
2023-06-02 17:31:55 -0300

    Ongoing project. Some details changed, like added observers

---
+++ campo-minado-swing/src/br/com/Mardor/cm/visao/Temp.java
@@ -0,0 +1,11 @@
+package br.com.Mardor.cm.visao;
+
+import br.com.Mardor.cm.modelo.Tabuleiro;
+
+public class Temp {
+	public static void main(String[] args) {
+	
+		Tabuleiro tabuleiro = new Tabuleiro(3, 3, 9);
+		tabuleiro.abrir(2, 2);
+	}
+}
